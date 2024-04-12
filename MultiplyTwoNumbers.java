5467613e4d8e62e0a807b77055fafa1b0fde15cd
Ashish Olkha <85562387+ashisholkha@users.noreply.github.com>
2024-04-12 20:58:23 +0530

    Create MultiplyTwoNumbers.java

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,14 @@
+// Write a code which takes two integers "F1" and "F2" as inputs and print their product .
+
+import java.util.Scanner;
+public class Solution
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long F1 = s.nextLong();
+		long F2 = s.nextLong();
+		long p = (F1*F2);
+		System.out.println((long) p);
+	
+	}
+}
