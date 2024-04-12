5bc5c1d35994967fa64bdbec2f96eb72045daa7e
Ashish Olkha <85562387+ashisholkha@users.noreply.github.com>
2024-04-12 20:59:43 +0530

    Create ASCIIvalue.java

---
+++ ASCIIvalue.java
@@ -0,0 +1,20 @@
+//		Given a character, you need to print the ASCII value of that character.
+//
+//		Example:
+//		Input: ch = ‘a’
+//
+//		Output: 97
+//
+//		Explanation: ASCII value of ‘a’ is 97.
+
+import java.util.Scanner;
+public class Solution
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char ch = s.next().charAt(0);
+		int p = ch;
+		System.out.println(p);
+	
+	}
+}
