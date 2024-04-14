c865e7f051b51edd38cdc65a087d5b0fcef6965f
Ashish Olkha <85562387+ashisholkha@users.noreply.github.com>
2024-04-14 19:02:24 +0530

    Create TriangularPattern1.java

---
+++ TriangularPattern1.java
@@ -0,0 +1,29 @@
+//		Print the following pattern for the given N number of rows.
+//
+//		Pattern for N = 4
+//		1
+//		12
+//		123
+//		1234
+
+import java.util.Scanner;
+
+public class TriangularPattern1 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		while(i<=n){
+			int j = 1;
+			while(j<=i){
+				System.out.print(j);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
